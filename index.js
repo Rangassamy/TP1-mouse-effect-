7b9a875003733f0c425ff0e3a856bff15c24f06d
Rangassamy <enzorangassamy@gmail.com>
2024-04-03 11:43:16 -0400

    added first version without correction

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+//--------------------------------------------------------------------------
+
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
+const box3 = document.querySelector(".box3");
+
+window.addEventListener("mousemove", (e) => {
+  box1.style.left = e.pageX + "px";
+  box1.style.top = e.pageY + "px";
+});
+window.addEventListener("mousemove", (e) => {
+  box2.style.left = e.pageX + "px";
+  box2.style.top = e.pageY + "px";
+});
+window.addEventListener("mousemove", (e) => {
+  box3.style.left = e.pageX + "px";
+  box3.style.top = e.pageY + "px";
+});
